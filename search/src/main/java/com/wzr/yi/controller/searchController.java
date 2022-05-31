0aba5f1353962c5f0bed91c2300e64dc42800e20
zhenrnewu <zhenrenwu@tencent.com>
2022-05-31 20:08:48 +0800

    能跑的版本

--- search/src/main/java/com/wzr/yi/controller/searchController.java
+++ search/src/main/java/com/wzr/yi/controller/searchController.java
@@ -22,6 +22,6 @@ public class searchController {
 
     @GetMapping("/testdb")
     public String testDb(){
-        return
+        return null;
     }
 }
--- search/src/main/java/com/wzr/yi/service/impl/DbServiceImpl.java
+++ search/src/main/java/com/wzr/yi/service/impl/DbServiceImpl.java
@@ -1,21 +1,21 @@
-package com.wzr.yi.service.impl;
-
-import com.alibaba.fastjson.JSON;
-import com.wzr.yi.service.DbService;
-
-import java.util.List;
-
-/**
- * @autor zhenrenwu
- * @date 2022/5/4 10:07 下午
- */
-public class DbServiceImpl implements DbService {
-    @Override
-    public List<JSON> getDb() {
-        String url = "jdbc:mysql://localhost:3306/test"
-
-
-
-        return null;
-    }
-}
+//package com.wzr.yi.service.impl;
+//
+//import com.alibaba.fastjson.JSON;
+//import com.wzr.yi.service.DbService;
+//
+//import java.util.List;
+//
+///**
+// * @autor zhenrenwu
+// * @date 2022/5/4 10:07 下午
+// */
+//public class DbServiceImpl implements DbService {
+//    @Override
+//    public List<JSON> getDb() {
+//        String url = "jdbc:mysql://localhost:3306/test"
+//
+//
+//
+//        return null;
+//    }
+//}
