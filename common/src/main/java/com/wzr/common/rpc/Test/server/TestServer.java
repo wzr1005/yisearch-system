ca71d78ba8d1fb0f70d28c4eba54dffc1fb69082
wzr1005 <784230508@qq.com>
2022-06-22 12:10:41 +0800

    引入Kafka，微服务待拆解

---
+++ common/src/main/java/com/wzr/common/rpc/Test/server/TestServer.java
@@ -0,0 +1,18 @@
+package com.wzr.yi.rpc.Test.server;
+
+import com.wzr.yi.rpc.IDL.HelloService;
+import com.wzr.yi.rpc.core.server.RpcServer;
+
+/**
+ * @autor zhenrenwu
+ * @date 2022/6/12 2:19 下午
+ */
+//public class TestServer {
+//    public static void main(String[] args) {
+//        RpcServer rpcServer = new RpcServer(); // 真正的rpcServer
+//        HelloService helloService = new HelloServiceImpl();
+//        rpcServer.register(helloService);  //向rpc server注册对象里面的所有方法
+//
+//        rpcServer.server(9001);
+//    }
+//}
