27c6af70ad1e847af8c427031fd96e5e43461508
wzr1005 <784230508@qq.com>
2022-06-24 17:09:45 +0800

    es集群以及kibana启动脚本

--- esbuild.sh
+++ esbuild.sh
@@ -3,3 +3,6 @@ cd /usr/local/Cellar/elasticsearch/
 node0/bin/elasticsearch -d
 node1/bin/elasticsearch -d
 node2/bin/elasticsearch -d
+wait
+cd /usr/local/Cellar/kibana-6.8.23-darwin-x86_64
+bin/kibanan
